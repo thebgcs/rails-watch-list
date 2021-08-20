562eec7bd463b0a1c4e4aafccd68e351b9a735eb
Bruno Santiago <br.gabriel10@gmail.com>
2021-08-20 16:54:04 +0200

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists do
-    resources :bookmarks, only: %i[create new]
+  root to: 'lists#index'
+  resources :lists, except: %i[edit update] do
+    resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[destroy]
 end
